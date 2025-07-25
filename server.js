e4001ed4c51b6d2ab7fcdbf6a9822efe8266d078
Mekd422 <mekdes.asefa-ug@aau.edu.et>
2025-07-25 11:56:35 +0300

    homepage

--- server.js
+++ server.js
@@ -2,11 +2,20 @@ const express = require("express")
 const app = express();
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
 
 //routes
 app.get("/", (req, res) => {
     res.render("homepage");
+});
+
+app.get("/login", (req, res) => {
+    res.render("login");   
+});
+
+app.post("/register", (req, res) => {
+
 });
 
 app.listen(3000, () => {
