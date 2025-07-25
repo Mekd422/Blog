c6cd3b668ff5484a05d9b0cbf42c9eb086fd73a3
Mekd422 <mekdes.asefa-ug@aau.edu.et>
2025-07-25 14:52:18 +0300

    username restriction

--- server.js
+++ server.js
@@ -15,6 +15,23 @@ app.get("/login", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
+    const errors = []
+
+    if(typeof req.body.username !== "string") req.body.username = "";
+    if(typeof req.body.password !== "string") req.body.password = "";
+
+    req.body.username = req.body.username.trim();
+
+    if(!req.body.username) errors.push("you must provide a username.");
+    if(req.body.username && req.body.username.length < 3) errors.push("username must be at least 3 characters.");
+    if(!req.body.username && req.body.username.length > 10) errors.push("username must be at most 10 characters."); 
+    if(req.body.username && !req.body.username.match(/^[a-zA-Z0-9]+$/)) errors.push("username can only contain letters and numbers.");
+
+    if(errors.length){
+        return res.render("homepage", { errors});
+    }else{
+        res.send("Thank you for registering, ");
+    }
 
 });
 
