cf51d1282a68187f902c520e5c424db15cf7b227
Mekd422 <mekdes.asefa-ug@aau.edu.et>
2025-07-26 09:33:21 +0300

    saving the user on the db

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express")
-const db = require("better-sqlite3")("blog.db");
+const bcrypt = require("bcrypt");
+const path = require("path");
+const db = require("better-sqlite3")(path.join(__dirname, "blog.db"));
 db.pragma("journal_mode = WAL");
 
 
@@ -8,8 +10,8 @@ const createTables = db.transaction(() => {
     db.prepare(
         `CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            username STRING NOT NULL UNIQUE,
-            password STRING NOT NULL,)`
+            username TEXT NOT NULL UNIQUE,
+            password TEXT NOT NULL)`
     ).run()
 })
 
@@ -41,20 +43,26 @@ app.post("/register", (req, res) => {
 
     if(!req.body.username) errors.push("you must provide a username.");
     if(req.body.username && req.body.username.length < 3) errors.push("username must be at least 3 characters.");
-    if(!req.body.username && req.body.username.length > 10) errors.push("username must be at most 10 characters."); 
+    if(req.body.username && req.body.username.length > 10) errors.push("username must be at most 10 characters."); 
     if(req.body.username && !req.body.username.match(/^[a-zA-Z0-9]+$/)) errors.push("username can only contain letters and numbers.");
 
     if(!req.body.password) errors.push("you must provide a password.");
     if(req.body.password && req.body.password.length < 6) errors.push("password must be at least 6 characters.");
-    if(!req.body.password && req.body.password.length > 12) errors.push("password must be at most 12 characters."); 
+    if(req.body.password && req.body.password.length > 12) errors.push("password must be at most 12 characters."); 
 
     if(errors.length){
         return res.render("homepage", { errors});
     }
 
     // save the new user into a database
+    const salt = bcrypt.genSaltSync(10);
+    req.body.password = bcrypt.hashSync(req.body.password, salt);
+    const ourStatement = db.prepare("INSERT INTO users (username, password) VALUES (?, ?)")
+    ourStatement.run(req.body.username, req.body.password);
 
     // log in the user in by giving then a cookie
+    
+    res.send("Thank you");
 
 });
 
